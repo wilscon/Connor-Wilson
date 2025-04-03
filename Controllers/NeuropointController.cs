e76d40a7a670f0dbd293226dc2e3505f6df1df55
unknown <connor.wilson48@gmail.com>
2025-04-02 18:28:36 -0700

    Added controller for redirecting user to neuropoint

---
+++ Controllers/NeuropointController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace Connor_Wilson.Controllers
+{
+    public class NeuropointController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        private string? emailAPIKEY = Environment.GetEnvironmentVariable("Website_Notification");
+
+        public NeuropointController(HttpClient httpClient) { 
+            _httpClient = httpClient;
+        }
+        public async Task<IActionResult> Index()
+        {
+            DateTime now = DateTime.UtcNow;
+            TimeZoneInfo pstZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            DateTime pstTime = TimeZoneInfo.ConvertTimeFromUtc(now, pstZone);  
+
+            var client = new SendGridClient(emailAPIKEY);
+            var from = new EmailAddress("connor.wilson48@gmail.com", "Connor Wilson");
+            var subject = "Neuropoint visited";
+            var to = new EmailAddress("connor.wilson48@gmail.com", "Connor Wilson");
+            var plainTextContent = "Somone has visited neuropoint from your website at " + pstTime;
+            var htmlContent = "<p>Somone has visited neuropoint from your website at " + pstTime + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var msgResponse = await client.SendEmailAsync(msg); 
+
+            return Redirect("https://neuro-point.com");
+        }
+    }
+}
